e01ff402c28b2f4737050cd6a36a2eab11520f6e
amoljagtap17 <amoljagtap17@gmail.com>
2023-10-09 20:00:47 +0530

    ecommerce-backend: add graphql setup to orders-app

--- apps/orders-app/src/orders-app.module.ts
+++ apps/orders-app/src/orders-app.module.ts
@@ -1,9 +1,29 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { HealthModule } from './health/health.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [OrdersModule, HealthModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+      sortSchema: true,
+      introspection: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      include: [HealthModule],
+    }),
+    OrdersModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
