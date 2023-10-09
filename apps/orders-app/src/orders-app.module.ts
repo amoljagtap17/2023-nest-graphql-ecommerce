e30f28c8221fabb1fef0f943469fa6442d775552
amoljagtap17 <amoljagtap17@gmail.com>
2023-10-09 19:52:52 +0530

    ecommerce-backend: remove boilerplate code from orders-app

--- apps/orders-app/src/orders-app.module.ts
+++ apps/orders-app/src/orders-app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { OrdersAppController } from './orders-app.controller';
-import { OrdersAppService } from './orders-app.service';
 
 @Module({
   imports: [],
-  controllers: [OrdersAppController],
-  providers: [OrdersAppService],
+  controllers: [],
+  providers: [],
 })
 export class OrdersAppModule {}
--- apps/orders-app/src/orders-app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class OrdersAppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
-}
